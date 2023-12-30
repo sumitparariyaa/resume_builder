81d2208fa1fb6016f318e713ac14c69c32cbb24f
sumitparariyaa <sk7778140@gmail.com>
2023-12-30 13:18:38 +0530

    setup Flutter empty project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,11 @@ import 'package:resume_builder/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+<<<<<<< HEAD
     await tester.pumpWidget(const MyApp());
+=======
+    await tester.pumpWidget(const ResumeBuilder());
+>>>>>>> abb62fd (setup Flutter empty project)
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
