c18cf33f29dbe25f135b349c0c350d6aadaeb52f
sumitparariyaa <sk7778140@gmail.com>
2023-12-30 16:45:52 +0530

    Added pdf viewer and generate pdf function to generate pdf

--- lib/screens/resume_screen.dart
+++ lib/screens/resume_screen.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:open_file/open_file.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import '../data_manager.dart';
 import '../models/resume_item.dart';
@@ -18,7 +24,6 @@ class _ResumeScreenState extends State<ResumeScreen> {
     super.initState();
     _loadResume();
   }
-
   _loadResume() async {
     List<ResumeItem> loadedResume = await ResumeDataManager.getResume();
     setState(() {
@@ -65,7 +70,6 @@ class _ResumeScreenState extends State<ResumeScreen> {
       _saveResume(); // Ensure _saveResume is declared before calling it
     }
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -116,14 +120,68 @@ class _ResumeScreenState extends State<ResumeScreen> {
       //     );
       //   },
       // ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
-        child: Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _addItem,
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 16.0),
+          FloatingActionButton(
+            onPressed: _generatePDF,
+            child: const Icon(Icons.picture_as_pdf),
+          ),
+        ],
       ),
     );
   }
+  _generatePDF() async {
+    final pdf = pw.Document();
 
+    // Custom fonts
+    final boldFont = pw.Font.ttf(await rootBundle.load("assets/fonts/OpenSans-Bold.ttf"));
+    final regularFont = pw.Font.ttf(await rootBundle.load("assets/fonts/OpenSans-Regular.ttf"));
 
+    // Styling
+    final titleStyle = pw.TextStyle(font: boldFont, fontSize: 16, color: PdfColors.blue900);
+    final contentStyle = pw.TextStyle(font: regularFont, fontSize: 14, color: PdfColors.black);
 
+    // Add content to the PDF
+    pdf.addPage(
+      pw.Page(
+        margin: pw.EdgeInsets.all(20.0),
+        build: (context) => pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Header(
+              level: 1,
+              text: 'Resume',
+              textStyle: pw.TextStyle(font: boldFont, fontSize: 24, color: PdfColors.blue900),
+            ),
+            pw.SizedBox(height: 20),
+            for (var item in resume)
+              pw.Column(
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  pw.Text(item.title, style: titleStyle),
+                  pw.Text(item.content, style: contentStyle),
+                  pw.SizedBox(height: 10),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
 
+    // Save the PDF to a file
+    final Directory tempDir = await getTemporaryDirectory();
+    final String tempPath = tempDir.path;
+    final String pdfPath = '$tempPath/resume.pdf';
+    final File file = File(pdfPath);
+    await file.writeAsBytes(pdf.save() as List<int>);
+
+    // Open the PDF using a PDF viewer
+    OpenFile.open(pdfPath);
+  }
 }
